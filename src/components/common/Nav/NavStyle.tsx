2d4d8e7012b99542ffc681f573a558bdb4d0de66
leeleeleeleejun <wnstjr6293@naver.com>
2023-11-23 13:29:37 +0900

    Design: nav 글쓰기 버튼 추가

--- src/components/common/Nav/NavStyle.tsx
+++ src/components/common/Nav/NavStyle.tsx
@@ -2,20 +2,25 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const NavBox = styled.div`
-  margin: 50px 0 25px;
-  padding: 10px 40px;
+  display: flex;
+  justify-content: space-between;
+  margin: 50px 40px 25px;
+  border-bottom: 1px solid var(--color-gray-3);
+  padding-bottom: 3px;
 
-  nav {
-    display: flex;
-    gap: 15px;
-    border-bottom: 1px solid var(--color-gray-3);
+  button {
+    padding: 4px 12px;
+    background: none;
+    border: 1px solid var(--color-gray);
+    border-radius: 8px;
+    font-size: 14px;
   }
 `;
 export const NavItem = styled(Link)`
   color: black;
   font-size: 21px;
   font-weight: var(--weight-semi-bold);
-  padding: 7px;
+  padding: 8px;
   text-decoration: none;
   border-bottom: 3px solid var(--color-gray-3);
 `;
