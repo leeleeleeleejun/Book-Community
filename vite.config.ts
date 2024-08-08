615665025ee0f991f9b82c82d20f0f403d32a64b
leeleeleeleejun <wnstjr6293@naver.com>
2024-08-08 19:05:43 +0900

    chore: vite-plugin-pwa 설치 및 pwa 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), VitePWA({ registerType: 'autoUpdate',
+    injectRegister: 'auto',
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+    },
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],})],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
